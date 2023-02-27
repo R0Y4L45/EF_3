c7f890f913e4f48e1467e88c2f9ddbfc49cb4dae
R0Y4L <royalb9514@gmail.com>
2023-02-28 02:11:09 +0400

    Initial

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using EntityFramework_3.Models;
+using EntityFramework_3.Repositories;
+
+namespace EntityFramework_3;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var unitOfWork = new UnitOfWork();
+
+        Student student1 = new Student()
+        {
+            FirstName = "Alim",
+            LastName = "Qasimov",
+            Id_Group = 1
+        };
+
+        unitOfWork.StudentRepository.Add(student1);
+        Console.WriteLine(student1.FirstName);
+        unitOfWork.StudentRepository.Remove(student1);
+        Console.WriteLine(student1.LastName);
+
+        unitOfWork.Save();;
+    }
+}
